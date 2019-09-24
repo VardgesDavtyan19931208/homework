bae75ec6101aa72d5ca2eda810f57eb99b66d5be
VardgesDavtyan19931208 <vardges.davtyan.1993@gmail.com>
2019-09-25 00:20:54 +0400

    Finally!

---
+++ even_or_odd.py
@@ -0,0 +1,7 @@
+name = input("Hello, what`s your name?: ")
+print("Nice to meet you! I will help you to clarify if the number you enterd is even or od :). So, if you see True its even, if false - odd.")
+
+number = input("Well, " + name + " , please input the number:")
+
+
+number_is_even = print(int(number)%2 == 0)
