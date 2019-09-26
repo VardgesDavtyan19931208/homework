6bdc3086cbfdccc803f716c1585c3979eb62b288
VardgesDavtyan19931208 <vardges.davtyan.1993@gmail.com>
2019-09-26 23:30:49 +0400

    check division by 5 and 11

---
+++ 5_and_11.py
@@ -0,0 +1,14 @@
+x = input("Hello, please input your number: ")
+
+b = float(x) % 55 == 0
+
+if b == True:
+	print("Your number is divisible by 5 and  11")
+else:
+	print("Your number isn`t divisible by 5 and  11")
+
+print("Good luck!")
+
+
+
+
