b001eac27f119b9fba845be56c4f42f576f94d05
VardgesDavtyan19931208 <vardges.davtyan.1993@gmail.com>
2019-09-29 22:23:37 +0400

    multiplication

---
+++ multiplication_for_any_number.py
@@ -0,0 +1,6 @@
+number_by_user = int(input("Please insert humber: "))
+
+range_by_user = int(input("Please insert range: "))
+
+for j in range(1, range_by_user):
+		print(number_by_user, "*", j, "equals", number_by_user*j)
